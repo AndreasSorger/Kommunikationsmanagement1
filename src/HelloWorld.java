f286aeb48666caee747f92eaccc5993545220f7a
Andreas <s52478@edu.campus02.at>
2022-09-16 19:25:01 +0200

    initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+
+    //psvm+TAB //main+TAB
+
+    public static void main(String[] args) {
+
+    //sout+TAB
+        System.out.println("Hello World");
+        System.out.println("Welccome to KONF 2022!");
+        System.out.println("Zeile 3");
+        System.out.println("Zeile 4");  // Strg+D
+
+    }
+}
